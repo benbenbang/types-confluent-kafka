6024232761ebcdf86900b2deabdb50bfede64b31
benbenbang <bn@bitbrew.dev>
2023-08-12 20:24:41 +0200

    feat: implement _util module typings

--- confluent_kafka-stubs/_util/conversion_util.pyi
+++ confluent_kafka-stubs/_util/conversion_util.pyi
@@ -2,3 +2,7 @@
 types-confluent-kafka: A package providing type hints for the confluent-kafka Python package.
 This package is licensed under the Apache 2.0 License.
 """
+
+class ConversionUtil:
+    @staticmethod
+    def convert_to_enum(val, enum_clazz): ...
