49f25915f3a32760f018d39ab5fcda9bb6b386b9
Ben Chen <bn@bitbrew.dev>
2023-08-12 12:20:18 +0200

    refactor: enhance cimpl typings (#13)

--- confluent_kafka-stubs/cimpl.pyi
+++ confluent_kafka-stubs/cimpl.pyi
@@ -2,19 +2,11 @@ from __future__ import annotations
 
 # standard library
 from types import TracebackType
-from typing import Any, Callable
+from typing import Any, Callable, ClassVar
 
-# Type Aliases
-ConfigDict = dict[str, Any]
-ClusterMetadata = Any
-KafkaValue = str | bytes
-KeyValueTuple = tuple[str, KafkaValue]
 # To-Do: the following aliases should be found in the c implementation
 ConsumerGroupState = object
-ListConsumerGroupsResult = object
 ConsumerGroupTopicPartitions = object
-ConsumerGroupDescription = object
-States = set[ConsumerGroupState]
 
 def libversion() -> tuple[str, int]: ...
 def version() -> tuple[str, int]: ...
@@ -69,168 +61,168 @@ TIMESTAMP_NOT_AVAILABLE: int
 
 class KafkaError:
     # Class Private Vars
-    _ALL_BROKERS_DOWN: int
-    _APPLICATION: int
-    _ASSIGNMENT_LOST: int
-    _ASSIGN_PARTITIONS: int
-    _AUTHENTICATION: int
-    _AUTO_OFFSET_RESET: int
-    _BAD_COMPRESSION: int
-    _BAD_MSG: int
-    _CONFLICT: int
-    _CRIT_SYS_RESOURCE: int
-    _DESTROY: int
-    _EXISTING_SUBSCRIPTION: int
-    _FAIL: int
-    _FATAL: int
-    _FENCED: int
-    _FS: int
-    _GAPLESS_GUARANTEE: int
-    _INCONSISTENT: int
-    _INTR: int
-    _INVALID_ARG: int
-    _INVALID_TYPE: int
-    _IN_PROGRESS: int
-    _ISR_INSUFF: int
-    _KEY_DESERIALIZATION: int
-    _KEY_SERIALIZATION: int
-    _LOG_TRUNCATION: int
-    _MAX_POLL_EXCEEDED: int
-    _MSG_TIMED_OUT: int
-    _NODE_UPDATE: int
-    _NOENT: int
-    _NOOP: int
-    _NOT_CONFIGURED: int
-    _NOT_IMPLEMENTED: int
-    _NO_OFFSET: int
-    _OUTDATED: int
-    _PARTIAL: int
-    _PARTITION_EOF: int
-    _PREV_IN_PROGRESS: int
-    _PURGE_INFLIGHT: int
-    _PURGE_QUEUE: int
-    _QUEUE_FULL: int
-    _READ_ONLY: int
-    _RESOLVE: int
-    _RETRY: int
-    _REVOKE_PARTITIONS: int
-    _SSL: int
-    _STATE: int
-    _TIMED_OUT: int
-    _TIMED_OUT_QUEUE: int
-    _TRANSPORT: int
-    _UNDERFLOW: int
-    _UNKNOWN_BROKER: int
-    _UNKNOWN_GROUP: int
-    _UNKNOWN_PARTITION: int
-    _UNKNOWN_PROTOCOL: int
-    _UNKNOWN_TOPIC: int
-    _UNSUPPORTED_FEATURE: int
-    _VALUE_DESERIALIZATION: int
-    _VALUE_SERIALIZATION: int
-    _WAIT_CACHE: int
-    _WAIT_COORD: int
+    _ALL_BROKERS_DOWN: ClassVar[int]
+    _APPLICATION: ClassVar[int]
+    _ASSIGNMENT_LOST: ClassVar[int]
+    _ASSIGN_PARTITIONS: ClassVar[int]
+    _AUTHENTICATION: ClassVar[int]
+    _AUTO_OFFSET_RESET: ClassVar[int]
+    _BAD_COMPRESSION: ClassVar[int]
+    _BAD_MSG: ClassVar[int]
+    _CONFLICT: ClassVar[int]
+    _CRIT_SYS_RESOURCE: ClassVar[int]
+    _DESTROY: ClassVar[int]
+    _EXISTING_SUBSCRIPTION: ClassVar[int]
+    _FAIL: ClassVar[int]
+    _FATAL: ClassVar[int]
+    _FENCED: ClassVar[int]
+    _FS: ClassVar[int]
+    _GAPLESS_GUARANTEE: ClassVar[int]
+    _INCONSISTENT: ClassVar[int]
+    _INTR: ClassVar[int]
+    _INVALID_ARG: ClassVar[int]
+    _INVALID_TYPE: ClassVar[int]
+    _IN_PROGRESS: ClassVar[int]
+    _ISR_INSUFF: ClassVar[int]
+    _KEY_DESERIALIZATION: ClassVar[int]
+    _KEY_SERIALIZATION: ClassVar[int]
+    _LOG_TRUNCATION: ClassVar[int]
+    _MAX_POLL_EXCEEDED: ClassVar[int]
+    _MSG_TIMED_OUT: ClassVar[int]
+    _NODE_UPDATE: ClassVar[int]
+    _NOENT: ClassVar[int]
+    _NOOP: ClassVar[int]
+    _NOT_CONFIGURED: ClassVar[int]
+    _NOT_IMPLEMENTED: ClassVar[int]
+    _NO_OFFSET: ClassVar[int]
+    _OUTDATED: ClassVar[int]
+    _PARTIAL: ClassVar[int]
+    _PARTITION_EOF: ClassVar[int]
+    _PREV_IN_PROGRESS: ClassVar[int]
+    _PURGE_INFLIGHT: ClassVar[int]
+    _PURGE_QUEUE: ClassVar[int]
+    _QUEUE_FULL: ClassVar[int]
+    _READ_ONLY: ClassVar[int]
+    _RESOLVE: ClassVar[int]
+    _RETRY: ClassVar[int]
+    _REVOKE_PARTITIONS: ClassVar[int]
+    _SSL: ClassVar[int]
+    _STATE: ClassVar[int]
+    _TIMED_OUT: ClassVar[int]
+    _TIMED_OUT_QUEUE: ClassVar[int]
+    _TRANSPORT: ClassVar[int]
+    _UNDERFLOW: ClassVar[int]
+    _UNKNOWN_BROKER: ClassVar[int]
+    _UNKNOWN_GROUP: ClassVar[int]
+    _UNKNOWN_PARTITION: ClassVar[int]
+    _UNKNOWN_PROTOCOL: ClassVar[int]
+    _UNKNOWN_TOPIC: ClassVar[int]
+    _UNSUPPORTED_FEATURE: ClassVar[int]
+    _VALUE_DESERIALIZATION: ClassVar[int]
+    _VALUE_SERIALIZATION: ClassVar[int]
+    _WAIT_CACHE: ClassVar[int]
+    _WAIT_COORD: ClassVar[int]
 
     # Class Vars
-    BROKER_NOT_AVAILABLE: int
-    CLUSTER_AUTHORIZATION_FAILED: int
-    CONCURRENT_TRANSACTIONS: int
-    COORDINATOR_LOAD_IN_PROGRESS: int
-    COORDINATOR_NOT_AVAILABLE: int
-    DELEGATION_TOKEN_AUTH_DISABLED: int
-    DELEGATION_TOKEN_AUTHORIZATION_FAILED: int
-    DELEGATION_TOKEN_EXPIRED: int
-    DELEGATION_TOKEN_NOT_FOUND: int
-    DELEGATION_TOKEN_OWNER_MISMATCH: int
-    DELEGATION_TOKEN_REQUEST_NOT_ALLOWED: int
-    DUPLICATE_RESOURCE: int
-    DUPLICATE_SEQUENCE_NUMBER: int
-    ELECTION_NOT_NEEDED: int
-    ELIGIBLE_LEADERS_NOT_AVAILABLE: int
-    FEATURE_UPDATE_FAILED: int
-    FENCED_INSTANCE_ID: int
-    FENCED_LEADER_EPOCH: int
-    FETCH_SESSION_ID_NOT_FOUND: int
-    GROUP_AUTHORIZATION_FAILED: int
-    GROUP_ID_NOT_FOUND: int
-    GROUP_MAX_SIZE_REACHED: int
-    GROUP_SUBSCRIBED_TO_TOPIC: int
-    ILLEGAL_GENERATION: int
-    ILLEGAL_SASL_STATE: int
-    INCONSISTENT_GROUP_PROTOCOL: int
-    INCONSISTENT_VOTER_SET: int
-    INVALID_COMMIT_OFFSET_SIZE: int
-    INVALID_CONFIG: int
-    INVALID_FETCH_SESSION_EPOCH: int
-    INVALID_GROUP_ID: int
-    INVALID_MSG: int
-    INVALID_MSG_SIZE: int
-    INVALID_PARTITIONS: int
-    INVALID_PRINCIPAL_TYPE: int
-    INVALID_PRODUCER_EPOCH: int
-    INVALID_PRODUCER_ID_MAPPING: int
-    INVALID_RECORD: int
-    INVALID_REPLICA_ASSIGNMENT: int
-    INVALID_REPLICATION_FACTOR: int
-    INVALID_REQUEST: int
-    INVALID_REQUIRED_ACKS: int
-    INVALID_SESSION_TIMEOUT: int
-    INVALID_TIMESTAMP: int
-    INVALID_TRANSACTION_TIMEOUT: int
-    INVALID_TXN_STATE: int
-    INVALID_UPDATE_VERSION: int
-    KAFKA_STORAGE_ERROR: int
-    LEADER_NOT_AVAILABLE: int
-    LISTENER_NOT_FOUND: int
-    LOG_DIR_NOT_FOUND: int
-    MEMBER_ID_REQUIRED: int
-    MSG_SIZE_TOO_LARGE: int
-    NETWORK_EXCEPTION: int
-    NO_ERROR: int
-    NON_EMPTY_GROUP: int
-    NO_REASSIGNMENT_IN_PROGRESS: int
-    NOT_CONTROLLER: int
-    NOT_COORDINATOR: int
-    NOT_ENOUGH_REPLICAS: int
-    NOT_ENOUGH_REPLICAS_AFTER_APPEND: int
-    NOT_LEADER_FOR_PARTITION: int
-    OFFSET_METADATA_TOO_LARGE: int
-    OFFSET_NOT_AVAILABLE: int
-    OFFSET_OUT_OF_RANGE: int
-    OPERATION_NOT_ATTEMPTED: int
-    OUT_OF_ORDER_SEQUENCE_NUMBER: int
-    POLICY_VIOLATION: int
-    PREFERRED_LEADER_NOT_AVAILABLE: int
-    PRINCIPAL_DESERIALIZATION_FAILURE: int
-    PRODUCER_FENCED: int
-    REASSIGNMENT_IN_PROGRESS: int
-    REBALANCE_IN_PROGRESS: int
-    RECORD_LIST_TOO_LARGE: int
-    REPLICA_NOT_AVAILABLE: int
-    REQUEST_TIMED_OUT: int
-    RESOURCE_NOT_FOUND: int
-    SASL_AUTHENTICATION_FAILED: int
-    SECURITY_DISABLED: int
-    STALE_BROKER_EPOCH: int
-    STALE_CTRL_EPOCH: int
-    THROTTLING_QUOTA_EXCEEDED: int
-    TOPIC_ALREADY_EXISTS: int
-    TOPIC_AUTHORIZATION_FAILED: int
-    TOPIC_DELETION_DISABLED: int
-    TOPIC_EXCEPTION: int
-    TRANSACTIONAL_ID_AUTHORIZATION_FAILED: int
-    TRANSACTION_COORDINATOR_FENCED: int
-    UNACCEPTABLE_CREDENTIAL: int
-    UNKNOWN: int
-    UNKNOWN_LEADER_EPOCH: int
-    UNKNOWN_MEMBER_ID: int
-    UNKNOWN_PRODUCER_ID: int
-    UNKNOWN_TOPIC_OR_PART: int
-    UNSTABLE_OFFSET_COMMIT: int
-    UNSUPPORTED_COMPRESSION_TYPE: int
-    UNSUPPORTED_FOR_MESSAGE_FORMAT: int
-    UNSUPPORTED_SASL_MECHANISM: int
-    UNSUPPORTED_VERSION: int
+    BROKER_NOT_AVAILABLE: ClassVar[int]
+    CLUSTER_AUTHORIZATION_FAILED: ClassVar[int]
+    CONCURRENT_TRANSACTIONS: ClassVar[int]
+    COORDINATOR_LOAD_IN_PROGRESS: ClassVar[int]
+    COORDINATOR_NOT_AVAILABLE: ClassVar[int]
+    DELEGATION_TOKEN_AUTH_DISABLED: ClassVar[int]
+    DELEGATION_TOKEN_AUTHORIZATION_FAILED: ClassVar[int]
+    DELEGATION_TOKEN_EXPIRED: ClassVar[int]
+    DELEGATION_TOKEN_NOT_FOUND: ClassVar[int]
+    DELEGATION_TOKEN_OWNER_MISMATCH: ClassVar[int]
+    DELEGATION_TOKEN_REQUEST_NOT_ALLOWED: ClassVar[int]
+    DUPLICATE_RESOURCE: ClassVar[int]
+    DUPLICATE_SEQUENCE_NUMBER: ClassVar[int]
+    ELECTION_NOT_NEEDED: ClassVar[int]
+    ELIGIBLE_LEADERS_NOT_AVAILABLE: ClassVar[int]
+    FEATURE_UPDATE_FAILED: ClassVar[int]
+    FENCED_INSTANCE_ID: ClassVar[int]
+    FENCED_LEADER_EPOCH: ClassVar[int]
+    FETCH_SESSION_ID_NOT_FOUND: ClassVar[int]
+    GROUP_AUTHORIZATION_FAILED: ClassVar[int]
+    GROUP_ID_NOT_FOUND: ClassVar[int]
+    GROUP_MAX_SIZE_REACHED: ClassVar[int]
+    GROUP_SUBSCRIBED_TO_TOPIC: ClassVar[int]
+    ILLEGAL_GENERATION: ClassVar[int]
+    ILLEGAL_SASL_STATE: ClassVar[int]
+    INCONSISTENT_GROUP_PROTOCOL: ClassVar[int]
+    INCONSISTENT_VOTER_SET: ClassVar[int]
+    INVALID_COMMIT_OFFSET_SIZE: ClassVar[int]
+    INVALID_CONFIG: ClassVar[int]
+    INVALID_FETCH_SESSION_EPOCH: ClassVar[int]
+    INVALID_GROUP_ID: ClassVar[int]
+    INVALID_MSG: ClassVar[int]
+    INVALID_MSG_SIZE: ClassVar[int]
+    INVALID_PARTITIONS: ClassVar[int]
+    INVALID_PRINCIPAL_TYPE: ClassVar[int]
+    INVALID_PRODUCER_EPOCH: ClassVar[int]
+    INVALID_PRODUCER_ID_MAPPING: ClassVar[int]
+    INVALID_RECORD: ClassVar[int]
+    INVALID_REPLICA_ASSIGNMENT: ClassVar[int]
+    INVALID_REPLICATION_FACTOR: ClassVar[int]
+    INVALID_REQUEST: ClassVar[int]
+    INVALID_REQUIRED_ACKS: ClassVar[int]
+    INVALID_SESSION_TIMEOUT: ClassVar[int]
+    INVALID_TIMESTAMP: ClassVar[int]
+    INVALID_TRANSACTION_TIMEOUT: ClassVar[int]
+    INVALID_TXN_STATE: ClassVar[int]
+    INVALID_UPDATE_VERSION: ClassVar[int]
+    KAFKA_STORAGE_ERROR: ClassVar[int]
+    LEADER_NOT_AVAILABLE: ClassVar[int]
+    LISTENER_NOT_FOUND: ClassVar[int]
+    LOG_DIR_NOT_FOUND: ClassVar[int]
+    MEMBER_ID_REQUIRED: ClassVar[int]
+    MSG_SIZE_TOO_LARGE: ClassVar[int]
+    NETWORK_EXCEPTION: ClassVar[int]
+    NO_ERROR: ClassVar[int]
+    NON_EMPTY_GROUP: ClassVar[int]
+    NO_REASSIGNMENT_IN_PROGRESS: ClassVar[int]
+    NOT_CONTROLLER: ClassVar[int]
+    NOT_COORDINATOR: ClassVar[int]
+    NOT_ENOUGH_REPLICAS: ClassVar[int]
+    NOT_ENOUGH_REPLICAS_AFTER_APPEND: ClassVar[int]
+    NOT_LEADER_FOR_PARTITION: ClassVar[int]
+    OFFSET_METADATA_TOO_LARGE: ClassVar[int]
+    OFFSET_NOT_AVAILABLE: ClassVar[int]
+    OFFSET_OUT_OF_RANGE: ClassVar[int]
+    OPERATION_NOT_ATTEMPTED: ClassVar[int]
+    OUT_OF_ORDER_SEQUENCE_NUMBER: ClassVar[int]
+    POLICY_VIOLATION: ClassVar[int]
+    PREFERRED_LEADER_NOT_AVAILABLE: ClassVar[int]
+    PRINCIPAL_DESERIALIZATION_FAILURE: ClassVar[int]
+    PRODUCER_FENCED: ClassVar[int]
+    REASSIGNMENT_IN_PROGRESS: ClassVar[int]
+    REBALANCE_IN_PROGRESS: ClassVar[int]
+    RECORD_LIST_TOO_LARGE: ClassVar[int]
+    REPLICA_NOT_AVAILABLE: ClassVar[int]
+    REQUEST_TIMED_OUT: ClassVar[int]
+    RESOURCE_NOT_FOUND: ClassVar[int]
+    SASL_AUTHENTICATION_FAILED: ClassVar[int]
+    SECURITY_DISABLED: ClassVar[int]
+    STALE_BROKER_EPOCH: ClassVar[int]
+    STALE_CTRL_EPOCH: ClassVar[int]
+    THROTTLING_QUOTA_EXCEEDED: ClassVar[int]
+    TOPIC_ALREADY_EXISTS: ClassVar[int]
+    TOPIC_AUTHORIZATION_FAILED: ClassVar[int]
+    TOPIC_DELETION_DISABLED: ClassVar[int]
+    TOPIC_EXCEPTION: ClassVar[int]
+    TRANSACTIONAL_ID_AUTHORIZATION_FAILED: ClassVar[int]
+    TRANSACTION_COORDINATOR_FENCED: ClassVar[int]
+    UNACCEPTABLE_CREDENTIAL: ClassVar[int]
+    UNKNOWN: ClassVar[int]
+    UNKNOWN_LEADER_EPOCH: ClassVar[int]
+    UNKNOWN_MEMBER_ID: ClassVar[int]
+    UNKNOWN_PRODUCER_ID: ClassVar[int]
+    UNKNOWN_TOPIC_OR_PART: ClassVar[int]
+    UNSTABLE_OFFSET_COMMIT: ClassVar[int]
+    UNSUPPORTED_COMPRESSION_TYPE: ClassVar[int]
+    UNSUPPORTED_FOR_MESSAGE_FORMAT: ClassVar[int]
+    UNSUPPORTED_SASL_MECHANISM: ClassVar[int]
+    UNSUPPORTED_VERSION: ClassVar[int]
 
     def __init__(
         self,
@@ -251,6 +243,13 @@ class KafkaError:
     def retriable(self) -> bool: ...
     def str(self) -> str: ...
     def txn_requires_abort(self) -> bool: ...
+    def __eq__(self, other) -> bool: ...
+    def __ge__(self, other) -> bool: ...
+    def __gt__(self, other) -> bool: ...
+    def __hash__(self) -> int: ...
+    def __le__(self, other) -> bool: ...
+    def __lt__(self, other) -> bool: ...
+    def __ne__(self, other) -> bool: ...
 
 class KafkaException(Exception):
     # Attributes
@@ -267,7 +266,34 @@ class KafkaException(Exception):
     def add_note(self, note: str) -> None: ...
     def with_traceback(self, traceback: TracebackType | None) -> Any: ...
 
-class _AdminClientImpl: ...
+class _AdminClientImpl:
+    """
+    Note:
+        - doc: https://docs.confluent.io/platform/current/clients/confluent-kafka-python/html/index.html#module-confluent_kafka.admin
+    """
+
+    def __init__(self, *args, **kwargs) -> None: ...
+    def alter_configs(self) -> Any: ...
+    def alter_consumer_group_offsets(self) -> Any: ...
+    def alter_user_scram_credentials(self) -> Any: ...
+    def create_acls(self) -> Any: ...
+    def create_partitions(self) -> Any: ...
+    def create_topics(self) -> Any: ...
+    def delete_acls(self) -> Any: ...
+    def delete_consumer_groups(self) -> Any: ...
+    def delete_topics(self) -> Any: ...
+    def describe_acls(self) -> Any: ...
+    def describe_configs(self) -> Any: ...
+    def describe_consumer_groups(self) -> Any: ...
+    def describe_user_scram_credentials(self) -> Any: ...
+    def incremental_alter_configs(self) -> Any: ...
+    def list_consumer_group_offsets(self) -> Any: ...
+    def list_consumer_groups(self) -> Any: ...
+    def list_groups(self, *args, **kwargs) -> Any: ...
+    def list_topics(self, *args, **kwargs) -> Any: ...
+    def poll(self) -> Any: ...
+    def set_sasl_credentials(self, username, password) -> Any: ...
+    def __len__(self) -> int: ...
 
 class Message:
     """
@@ -277,18 +303,19 @@ class Message:
 
     # Methods
     def error(self) -> KafkaError | None: ...
-    def headers(self) -> list[KeyValueTuple] | None: ...
-    def key(self) -> KafkaValue | None: ...
+    def headers(self) -> list[tuple[str, str | bytes]] | None: ...
+    def key(self) -> str | bytes | None: ...
     def latency(self) -> float | None: ...
     def leader_epoch(self) -> int | None: ...
     def offset(self) -> int | None: ...
     def partition(self) -> int | None: ...
-    def set_headers(self, value: list[KeyValueTuple]) -> None: ...
-    def set_key(self, value: KafkaValue) -> None: ...
-    def set_value(self, value: KafkaValue) -> None: ...
+    def set_headers(self, value: list[tuple[str, str | bytes]]) -> None: ...
+    def set_key(self, value: str | bytes) -> None: ...
+    def set_value(self, value: str | bytes) -> None: ...
     def timestamp(self) -> tuple[int, int]: ...
     def topic(self) -> str | None: ...
-    def value(self) -> KafkaValue | None: ...
+    def value(self) -> str | bytes | None: ...
+    def __len__(self) -> int: ...
 
 class NewTopic:
     def __init__(
@@ -304,7 +331,13 @@ class NewTopic:
             - doc: https://docs.confluent.io/platform/current/clients/confluent-kafka-python/html/index.html#newtopic
             - config reference: http://kafka.apache.org/documentation.html#topicconfigs
         """
-        ...
+        def __eq__(self, other) -> bool: ...
+        def __ge__(self, other) -> bool: ...
+        def __gt__(self, other) -> bool: ...
+        def __hash__(self) -> int: ...
+        def __le__(self, other) -> bool: ...
+        def __lt__(self, other) -> bool: ...
+        def __ne__(self, other) -> bool: ...
 
 class NewPartitions:
     def __init__(self, topic: str, new_total_count: int, replica_assignment: list[list[str]] | None = None) -> None:
@@ -312,7 +345,13 @@ class NewPartitions:
         Note:
             - doc: https://docs.confluent.io/platform/current/clients/confluent-kafka-python/html/index.html#newpartitions
         """
-        ...
+        def __eq__(self, other) -> bool: ...
+        def __ge__(self, other) -> bool: ...
+        def __gt__(self, other) -> bool: ...
+        def __hash__(self) -> int: ...
+        def __le__(self, other) -> bool: ...
+        def __lt__(self, other) -> bool: ...
+        def __ne__(self, other) -> bool: ...
 
 class TopicPartition:
     def __init__(
@@ -327,7 +366,13 @@ class TopicPartition:
         Note:
             - doc: https://docs.confluent.io/platform/current/clients/confluent-kafka-python/html/index.html#topicpartition
         """
-        ...
+        def __eq__(self, other) -> bool: ...
+        def __ge__(self, other) -> bool: ...
+        def __gt__(self, other) -> bool: ...
+        def __hash__(self) -> int: ...
+        def __le__(self, other) -> bool: ...
+        def __lt__(self, other) -> bool: ...
+        def __ne__(self, other) -> bool: ...
 
 class Producer:
     """
@@ -337,7 +382,7 @@ class Producer:
     """
 
     # Methods
-    def __init__(self, config: ConfigDict) -> None: ...
+    def __init__(self, config: dict[str, Any]) -> None: ...
     def abort_transaction(self, timeout: float | None = None) -> None: ...
     def begin_transaction(self) -> None: ...
     def commit_transaction(self, timeout: float | None = None) -> None:
@@ -358,7 +403,7 @@ class Producer:
             - timeout unit - second
         """
         ...
-    def list_topics(self, topic: str | None = None, timeout: float = -1) -> ClusterMetadata: ...
+    def list_topics(self, topic: str | None = None, timeout: float = -1) -> Any: ...
     def poll(self, timeout: float | None = None) -> int:
         """
         Note:
@@ -368,12 +413,12 @@ class Producer:
     def produce(
         self,
         topic: str,
-        value: KafkaValue | None = None,
-        key: KafkaValue | None = None,
+        value: str | bytes | None = None,
+        key: str | bytes | None = None,
         partition: int | None = None,
         on_delivery: Callable | None = None,
         timestamp: int = 0,
-        headers: dict[str, KafkaValue] | list[tuple[str, KafkaValue]] | None = None,
+        headers: dict[str, str | bytes] | list[tuple[str, str | bytes]] | None = None,
     ) -> None: ...
     def purge(self, in_queue: bool = True, in_flight: bool = True, blocking: bool = True) -> None: ...
     def send_offsets_to_transactions(
@@ -388,7 +433,7 @@ class Producer:
     def __len__(self) -> int: ...
 
 class Consumer:
-    def __init__(self, config: ConfigDict) -> None: ...
+    def __init__(self, config: dict[str, Any]) -> None: ...
     def assign(self, partitions: list[TopicPartition]) -> None: ...
     def assignment(self) -> list[TopicPartition]: ...
     def close(self) -> None: ...
@@ -408,7 +453,7 @@ class Consumer:
     ) -> tuple[int, int]: ...
     def incremental_assign(self, partitions: list[TopicPartition]) -> None: ...
     def incremental_unassign(self, partitions: list[TopicPartition]) -> None: ...
-    def list_topics(self, topics: str | None = None, timeout: float | int = -1) -> ClusterMetadata: ...
+    def list_topics(self, topics: str | None = None, timeout: float | int = -1) -> Any: ...
     def memberid(self) -> str | None: ...
     def offsets_for_times(
         self, partitions: list[TopicPartition], timeout: float | int | None = None
