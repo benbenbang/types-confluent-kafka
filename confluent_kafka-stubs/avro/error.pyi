8d8ebec88691b0d7b0cf00a1b0250bfd92d5f049
benbenbang <bn@bitbrew.dev>
2023-08-20 17:42:03 +0200

    feat: implement avro error typings

--- confluent_kafka-stubs/avro/error.pyi
+++ confluent_kafka-stubs/avro/error.pyi
@@ -2,3 +2,17 @@
 types-confluent-kafka: A package providing type hints for the confluent-kafka Python package.
 This package is licensed under the Apache 2.0 License.
 """
+
+# standard library
+from typing import ClassVar
+
+# pypi/conda library
+from confluent_kafka.cimpl import Message
+
+class ClientError(Exception):
+    """Error thrown by Schema Registry clients"""
+
+    message: ClassVar[Message]
+    http_code: ClassVar[int]
+
+    def __init__(self, message, http_code: int | None = None) -> None: ...
