c923a06b34e9b67c0a99dd65018d02bad41fa89d
Ben Chen <bn@bitbrew.dev>
2023-08-26 09:43:36 +0200

    feat: implement schema registry module entrypoint & errors typings (#106)

--- confluent_kafka-stubs/schema_registry/__init__.pyi
+++ confluent_kafka-stubs/schema_registry/__init__.pyi
@@ -2,3 +2,13 @@
 types-confluent-kafka: A package providing type hints for the confluent-kafka Python package.
 This package is licensed under the Apache 2.0 License.
 """
+
+# from .schema_registry_client import RegisteredSchema as RegisteredSchema
+# from .schema_registry_client import Schema as Schema
+# from .schema_registry_client import SchemaReference as SchemaReference
+# from .schema_registry_client import SchemaRegistryClient as SchemaRegistryClient
+# from .schema_registry_client import SchemaRegistryError as SchemaRegistryError
+
+def topic_subject_name_strategy(ctx, record_name): ...
+def topic_record_subject_name_strategy(ctx, record_name): ...
+def record_subject_name_strategy(ctx, record_name): ...
