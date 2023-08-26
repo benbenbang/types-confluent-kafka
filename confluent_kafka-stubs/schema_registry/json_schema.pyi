bc224a9c142214d4fb998803d6cda94e5e87a6a4
Ben Chen <bn@bitbrew.dev>
2023-08-26 16:24:31 +0200

    refactor: replace serialization ctx unkown type (#126)

--- confluent_kafka-stubs/schema_registry/json_schema.pyi
+++ confluent_kafka-stubs/schema_registry/json_schema.pyi
@@ -8,13 +8,13 @@ from __future__ import annotations
 from io import BytesIO
 from typing import Callable
 
-# pypi/conda library
-from confluent_kafka.schema_registry import Schema as Schema
-from confluent_kafka.schema_registry import topic_subject_name_strategy as topic_subject_name_strategy
-from confluent_kafka.schema_registry.schema_registry_client import SchemaRegistryClient
-from confluent_kafka.serialization import Deserializer as Deserializer
-from confluent_kafka.serialization import SerializationError as SerializationError
-from confluent_kafka.serialization import Serializer as Serializer
+from ..schema_registry import Schema as Schema
+from ..schema_registry import topic_subject_name_strategy as topic_subject_name_strategy
+from ..schema_registry.schema_registry_client import SchemaRegistryClient
+from ..serialization import Deserializer as Deserializer
+from ..serialization import SerializationContext
+from ..serialization import SerializationError as SerializationError
+from ..serialization import Serializer as Serializer
 
 class _ContextStringIO(BytesIO):
     def __enter__(self) -> _ContextStringIO: ...
@@ -25,16 +25,16 @@ class JSONSerializer(Serializer):
         self,
         schema_str: str | Schema,
         schema_registry_client: SchemaRegistryClient,
-        to_dict: Callable[[object], "SerializationContext"] | None = None,  # type: ignore
+        to_dict: Callable[[object], SerializationContext] | None = None,
         conf: dict | None = None,
     ) -> None: ...
-    def __call__(self, obj: object, ctx: "SerializationContext"): ...  # type: ignore
+    def __call__(self, obj: object, ctx: SerializationContext): ...  # type: ignore # issue: https://github.com/confluentinc/confluent-kafka-python/issues/1631
 
 class JSONDeserializer(Deserializer):
     def __init__(
         self,
         schema_str: str | Schema,
-        from_dict: Callable[[dict], "SerializationContext"] | None = ...,  # type: ignore
+        from_dict: Callable[[dict], SerializationContext] | None = ...,
         schema_registry_client: SchemaRegistryClient | None = None,
     ) -> None: ...
-    def __call__(self, data: bytes, ctx: "SerializationContext"): ...  # type: ignore
+    def __call__(self, data: bytes, ctx: SerializationContext): ...  # type: ignore # issue: https://github.com/confluentinc/confluent-kafka-python/issues/1631
--- confluent_kafka-stubs/schema_registry/protobuf.pyi
+++ confluent_kafka-stubs/schema_registry/protobuf.pyi
@@ -7,9 +7,8 @@ from __future__ import annotations
 # standard library
 import io
 
-# pypi/conda library
-from confluent_kafka.serialization import SerializationError as SerializationError
-
+from ..serialization import SerializationContext
+from ..serialization import SerializationError as SerializationError
 from . import reference_subject_name_strategy as reference_subject_name_strategy
 from . import topic_subject_name_strategy as topic_subject_name_strategy
 from .schema_registry_client import Schema as Schema
@@ -22,8 +21,8 @@ class _ContextStringIO(io.BytesIO):
 
 class ProtobufSerializer:
     def __init__(self, msg_type: "GeneratedProtocolMessageType", schema_registry_client: SchemaRegistryClient, conf: dict | None = None) -> None: ...  # type: ignore
-    def __call__(self, message: "Message", ctx: "SerializationContext"): ...  # type: ignore
+    def __call__(self, message: "Message", ctx: SerializationContext): ...  # type: ignore
 
 class ProtobufDeserializer:
     def __init__(self, message_type: "MessageType", conf: dict | None = None) -> None: ...  # type: ignore
-    def __call__(self, data: bytes, ctx: "SerializationContext"): ...  # type: ignore
+    def __call__(self, data: bytes, ctx: SerializationContext): ...
