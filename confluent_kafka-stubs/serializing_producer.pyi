5c3d695f5099b29ad414a5e02c1b05ebd859730c
benbenbang <bn@bitbrew.dev>
2023-08-26 17:23:05 +0200

    feat: implement serializing producer typings

--- confluent_kafka-stubs/serializing_producer.pyi
+++ confluent_kafka-stubs/serializing_producer.pyi
@@ -4,4 +4,25 @@ This package is licensed under the Apache 2.0 License.
 """
 from __future__ import annotations
 
-class SerializingProducer: ...
+# standard library
+from typing import Callable
+
+from .cimpl import KafkaError, Message
+from .cimpl import Producer as _ProducerImpl
+from .error import KeySerializationError as KeySerializationError
+from .error import ValueSerializationError as ValueSerializationError
+from .serialization import MessageField as MessageField
+from .serialization import SerializationContext as SerializationContext
+
+class SerializingProducer(_ProducerImpl):
+    def __init__(self, conf) -> None: ...
+    def produce(
+        self,
+        topic: str,
+        key: object | None = None,
+        value: object | None = None,
+        partition: int = -1,
+        on_delivery: Callable[[KafkaError], Message] | None = None,
+        timestamp: float = 0,
+        headers: dict[str, bytes | None] | None = None,
+    ) -> None: ...
