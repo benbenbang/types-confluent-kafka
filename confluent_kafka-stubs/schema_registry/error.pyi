3ef281e303a626d0735a12aeb77e94c449f2ee33
benbenbang <bn@bitbrew.dev>
2023-08-26 09:06:25 +0200

    feat: implement schema registry module entrypoint & errors typings

--- confluent_kafka-stubs/schema_registry/error.pyi
+++ confluent_kafka-stubs/schema_registry/error.pyi
@@ -2,3 +2,19 @@
 types-confluent-kafka: A package providing type hints for the confluent-kafka Python package.
 This package is licensed under the Apache 2.0 License.
 """
+
+# standard library
+from typing import ClassVar
+
+# pypi/conda library
+from fastavro.schema import SchemaParseException as SchemaParseException
+from fastavro.schema import UnknownType as UnknownType
+
+class SchemaRegistryError(Exception):
+    http_status_code: ClassVar[int]
+    error_code: ClassVar[int]
+    error_message: ClassVar[str]
+
+    UNKNOWN: int = -1
+
+    def __init__(self, http_status_code, error_code, error_message) -> None: ...
