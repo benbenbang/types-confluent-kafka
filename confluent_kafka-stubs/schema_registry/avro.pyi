04ae025ebb588f9342986820a0ab017c2227ee88
benbenbang <bn@bitbrew.dev>
2024-04-20 22:42:52 +0200

    fix: avro & json (de)serializer return types

--- confluent_kafka-stubs/schema_registry/avro.pyi
+++ confluent_kafka-stubs/schema_registry/avro.pyi
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 # standard library
 from io import BytesIO
-from typing import Any, Callable
+from typing import TYPE_CHECKING, Any, Callable
 
 from ..serialization import Deserializer as Deserializer
 from ..serialization import SerializationContext
@@ -15,6 +15,10 @@ from ..serialization import Serializer as Serializer
 from . import Schema as Schema
 from . import topic_subject_name_strategy as topic_subject_name_strategy
 from .schema_registry_client import SchemaRegistryClient
+
+if TYPE_CHECKING:
+    # pypi/conda library
+    from fastavro.types import AvroMessage as AvroMessage
 
 class _ContextStringIO(BytesIO):
     def __enter__(self) -> _ContextStringIO: ...
@@ -28,7 +32,7 @@ class AvroSerializer(Serializer):
         to_dict: Callable[[object, SerializationContext], dict[Any, Any]] | None = None,
         conf: dict | None = None,
     ) -> None: ...
-    def __call__(self, obj: object, ctx: SerializationContext) -> None: ...  # type: ignore # issue: https://github.com/confluentinc/confluent-kafka-python/issues/1631
+    def __call__(self, obj: object, ctx: SerializationContext) -> bytes | None: ...  # type: ignore # issue: https://github.com/confluentinc/confluent-kafka-python/issues/1631
 
 class AvroDeserializer(Deserializer):
     def __init__(
@@ -38,4 +42,4 @@ class AvroDeserializer(Deserializer):
         from_dict: Callable[[dict[Any, Any], SerializationContext], object] | None = None,
         return_record_name: bool = False,
     ) -> None: ...
-    def __call__(self, data: bytes, ctx: SerializationContext) -> None: ...  # type: ignore # issue: https://github.com/confluentinc/confluent-kafka-python/issues/1631
+    def __call__(self, data: bytes, ctx: SerializationContext) -> AvroMessage | dict | None: ...  # type: ignore # issue: https://github.com/confluentinc/confluent-kafka-python/issues/1631
