4136a438799dc9474f3bcb2476980b65a713291d
benbenbang <bn@bitbrew.dev>
2024-04-20 21:39:18 +0200

    refactor(admin): drop classvar

--- confluent_kafka-stubs/admin/_group.pyi
+++ confluent_kafka-stubs/admin/_group.pyi
@@ -4,9 +4,6 @@ This package is licensed under the Apache 2.0 License.
 """
 from __future__ import annotations
 
-# standard library
-from typing import ClassVar
-
 from .._model import ConsumerGroupState as ConsumerGroupState
 from .._model import Node as Node
 from .._util import ConversionUtil as ConversionUtil
@@ -14,9 +11,9 @@ from ..cimpl import TopicPartition as TopicPartition
 from ..error import KafkaException as KafkaException
 
 class ConsumerGroupListing:
-    group_id: ClassVar[str]
-    is_simple_consumer_group: ClassVar[bool]
-    state: ClassVar[ConsumerGroupState]
+    group_id: str
+    is_simple_consumer_group: bool
+    state: ConsumerGroupState
 
     def __init__(
         self,
@@ -26,8 +23,8 @@ class ConsumerGroupListing:
     ) -> None: ...
 
 class ListConsumerGroupsResult:
-    valid: ClassVar[list[ConsumerGroupListing]]
-    errors: ClassVar[list[KafkaException]]
+    valid: list[ConsumerGroupListing]
+    errors: list[KafkaException]
 
     def __init__(
         self,
@@ -36,16 +33,16 @@ class ListConsumerGroupsResult:
     ) -> None: ...
 
 class MemberAssignment:
-    topic_partitions: ClassVar[list[TopicPartition]]
+    topic_partitions: list[TopicPartition]
 
     def __init__(self, topic_partitions: list[TopicPartition] | None = None) -> None: ...
 
 class MemberDescription:
-    member_id: ClassVar[str]
-    client_id: ClassVar[str]
-    host: ClassVar[str]
-    assignment: ClassVar[MemberAssignment]
-    group_instance_id: ClassVar[str]
+    member_id: str
+    client_id: str
+    host: str
+    assignment: MemberAssignment
+    group_instance_id: str
 
     def __init__(
         self,
@@ -57,12 +54,12 @@ class MemberDescription:
     ) -> None: ...
 
 class ConsumerGroupDescription:
-    group_id: ClassVar[str]
-    is_simple_consumer_group: ClassVar[bool]
-    members: ClassVar[list[MemberDescription]]
-    partition_assignor: ClassVar[str]
-    state: ClassVar[ConsumerGroupState]
-    coordinator: ClassVar[Node]
+    group_id: str
+    is_simple_consumer_group: bool
+    members: list[MemberDescription]
+    partition_assignor: str
+    state: ConsumerGroupState
+    coordinator: Node
 
     def __init__(
         self,
