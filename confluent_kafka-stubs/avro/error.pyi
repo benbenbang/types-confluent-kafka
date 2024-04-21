aae5b07aa82476268dbe80d4f3f0c1a5f3b718f3
benbenbang <bn@bitbrew.dev>
2024-04-21 13:19:53 +0200

    refactor(avro): drop classvar in avro module

--- confluent_kafka-stubs/avro/error.pyi
+++ confluent_kafka-stubs/avro/error.pyi
@@ -4,13 +4,10 @@ This package is licensed under the Apache 2.0 License.
 """
 from __future__ import annotations
 
-# standard library
-from typing import ClassVar
-
 from ..cimpl import Message
 
 class ClientError(Exception):
-    message: ClassVar[Message]
-    http_code: ClassVar[int]
+    message: Message
+    http_code: int
 
     def __init__(self, message, http_code: int | None = None) -> None: ...
