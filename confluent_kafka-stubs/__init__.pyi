6449606c1919ccdb062c9c76a9ce558f6a81da27
Ben Chen <bn@bitbrew.dev>
2024-10-14 21:25:43 +0200

    fix(lint): apply ruff format (#214)
    ### **Description:**
    This pull request includes several changes to the
    `confluent_kafka-stubs` package, primarily focusing on adding future
    annotations and minor formatting adjustments.
    
    
    ### Formatting Adjustments:
    
    * Cleaned up indentation & added `from __future__ import annotations` to
    multiple files to enable postponed evaluation of type annotations. This
    change improves forward compatibility and type hinting performance.
    
    * Added trailing commas to the `AvroProducer` and `AvroConsumer` class
    constructors to improve code readability and maintainability.
    
    Please delete options that are not relevant.
    - [x] Bug fix (non-breaking change which fixes an issue)
    
    ### **Checklist:**
    
    - [x] All code follows the project's coding style and conventions.
    - [x] I have performed a self-review of my own code.
    - [x] My changes generate no new warnings or errors.
    - [x] I have commented my code, particularly in hard-to-understand
    areas.
    - [x] I have made corresponding updates to the documentation.

--- confluent_kafka-stubs/__init__.pyi
+++ confluent_kafka-stubs/__init__.pyi
@@ -2,6 +2,7 @@
 types-confluent-kafka: A package providing type hints for the confluent-kafka Python package.
 This package is licensed under the Apache 2.0 License.
 """
+
 from __future__ import annotations
 
 from ._model import ConsumerGroupState as ConsumerGroupState
