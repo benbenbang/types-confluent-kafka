f6d80ca05a1fe15149cf0fafcecea274a4b97d06
Andrew Sibley <andrew.sibley@field.energy>
2025-02-14 10:19:13 +0000

    Expand github actions tests to check python 3.12 & 3.13

--- .github/docker/build.sh
+++ .github/docker/build.sh
@@ -1,7 +1,7 @@
 #! /bin/env bash
 
 function build_images() {
-    local PYTHON_VERSIONS=(3.9 3.10 3.11)
+    local PYTHON_VERSIONS=(3.9 3.10 3.11 3.12 3.13)
     local POETRY_VERSION=1.5.1
     local REPOSITORY=benbenbang/types-confluent-kafka
 
